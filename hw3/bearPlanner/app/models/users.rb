a8408c090a6fc1fab050ec44276ec69d596db9fd
Rohan Cribbs <rohan.cribbs@gmail.com>
2012-02-16 16:02:31 -0800

    Added usernam and password presence requirement to user model. Generated calendar and event model/migrate stubs.

--- hw3/bearPlanner/app/models/users.rb
+++ hw3/bearPlanner/app/models/users.rb
@@ -1,3 +1,5 @@
 class Users < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true #Makes sure all useres have a unique name
+  validates :password, :presence => true
+  validates :name, :presence => true
 end
