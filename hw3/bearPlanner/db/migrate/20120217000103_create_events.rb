bdd8cb65157936bf2bd30622b99779379e1dec26
Rohan Cribbs <rohan.cribbs@gmail.com>
2012-02-25 15:47:24 -0800

    Fixed missing colons.

--- hw3/bearPlanner/db/migrate/20120217000103_create_events.rb
+++ hw3/bearPlanner/db/migrate/20120217000103_create_events.rb
@@ -1,9 +1,9 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
-        t.string event_name #Create the event name column
-        t.integer start_time #Create the start time column
-        t.integer end_time #Create the end time column
+        t.string :event_name #Create the event name column
+        t.integer :start_time #Create the start time column
+        t.integer :end_time #Create the end time column
       t.timestamps
     end
   end
